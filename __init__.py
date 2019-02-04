3914dc45032e905715732793144c1efa202f78ce
jprakke <JoshPrakke@gmail.com>
2019-02-04 02:50:53 -0500

    Updated api calls in __init__.py

--- __init__.py
+++ __init__.py
@@ -6,25 +6,47 @@ import simplejson as json
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func, MetaData
+from sqlalchemy.pool import StaticPool
 
 #Set up the database
 #source activate Project3ENV
 
 connection_string = (f"{username}:{password}@127.0.0.1:3306/global_terrorism_db")
 
-engine = create_engine(f'mysql://{connection_string}',encoding='utf-8')
+engine = create_engine(f'mysql://{connection_string}'
+    ,encoding='utf-8'
+    ,poolclass=StaticPool)
 
 Base = automap_base()
 Base.prepare(engine, reflect=True)
 
 GTD = Base.classes.global_terrorism_new
-# Happiness = Base.classes.world_happiness_index
+Happiness = Base.classes.world_happiness_index
 
 session = Session(engine)
 
 # Flask Setup
 
 app = Flask(__name__)
+
+queries =[
+    GTD.iyear,
+    GTD.country_txt,
+    GTD.latitude,
+    GTD.longitude,
+    GTD.attacktype1_txt,
+    GTD.attacktype2_txt,
+    GTD.attacktype3_txt,
+    GTD.targtype1_txt,
+    GTD.targsubtype1_txt,
+    GTD.gname,
+    GTD.weaptype1_txt,
+    GTD.weapsubtype1_txt,
+    GTD.weaptype2_txt,
+    GTD.weapsubtype2_txt,
+    GTD.nkill,
+    GTD.nwound
+    ]
 
 # Routing
 # @TODO route api correctly once database is created
@@ -44,16 +66,61 @@ def api():
         #"api/v1.0/Longitude",
     ]
     return render_template("api.html")
-@app.route("/api/v1.0/<year>")
-def year(year):
 
-    """ json of data by year """
-    queries =[GTD.latitude,GTD.longitude]
+@app.route("/api/v1.0/global_terror/metadata/<year>")
+def meta_data(year):
+    """ Return Meta Data for given year """
     results = session.query(*queries).\
-    filter(GTD.iyear >= year).all()
-    print(results)
-    return json.dumps(results)
+    filter(GTD.iyear == year).all()
+# TODO : Work out meta data table queries
+    year_metadata = {}
+    for i in results:
+        year_metadata["Year"] = i[0]
+        year_metadata["Country"] = i[1]
+        year_metadata["Attack Type 1"] = i[4]
+        year_metadata["Attack Type 2"] = i[5]
+        year_metadata["Attack Type 3"] = i[6]
+        year_metadata["Target Type"] = i[7]
+        year_metadata["Target Subtype"] =i[8]
+    
+    print(year_metadata)
+    return jsonify(year_metadata)
 
+@app.route("/api/v1.0/global_terror/<year>")
+def y(year):
+    """ json of data by year """
+
+    results = session.query(*queries).\
+        filter(GTD.iyear == year).all()
+    
+    return jsonify(results)
+
+@app.route("/api/v1.0/global_terror/<year>/<weaptype>")
+def w_y(year,weaptype):
+    """ json of data by year and weapon type """
+#    TODO: work on querying weapontype 2 could build api to have third input? ask group1
+    results = session.query(*queries).\
+        filter(GTD.iyear == year).\
+        filter(GTD.weaptype1_txt == weaptype).all()
+    
+    return jsonify(results)
+
+@app.route("/api/v1.0/happiness/<year>")
+def happiness(year):
+    """ json of average global happiness per year """
+    results = session.query(func.avg(Happiness.life_ladder).label("average")).\
+        filter(Happiness.year == year).all()
+    print(results[0])
+    return jsonify(results)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+  # ts = {
+    #     "Country":{"name":GTD.country_txt,
+    #                "Location":[GTD.latitude,
+    #                            GTD.longitude]
+                               
+    #                 }
+    #     }
