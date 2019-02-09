7699cc5c26b49b46b7befa18ad71d28680263254
Jollibee <j.botardo@gmail.com>
2019-02-08 19:08:42 -0500

    uncommented simplejson import on my local

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, render_template
 from settings import username, password
 import sqlalchemy
-# import simplejson as json
+import simplejson as json
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func, desc
