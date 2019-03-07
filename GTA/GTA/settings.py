ac456f03ffcf2c3afa6f953f1f68afda7a628936
jprakke <JoshPrakke@gmail.com>
2019-03-07 12:50:58 -0500

    fixed index.html pathing added views and urls

--- GTA/GTA/settings.py
+++ GTA/GTA/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'front_end',
+    'back_end',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
