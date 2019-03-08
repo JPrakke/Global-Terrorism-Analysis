7b7c573abf382e9a2803933b8f0620e15285ca7d
Timothy Sharpe <timothysharpe1295@gmail.com>
2019-03-07 20:51:10 -0500

    removed unneccesary js

--- GTA/front_end/static/front_end/js/dash.js
+++ GTA/front_end/static/front_end/js/dash.js
@@ -245,188 +245,188 @@
   return HeatmapOverlay;
 });
 
-const buildGauge = year =>{
-  d3.json(`/api/v1.0/happiness/${year}`).then(d=>{
-    let ts2 = Object.values(d);
-    let level = parseFloat(ts2) * 20;
+// const buildGauge = year =>{
+//   d3.json(`/api/v1.0/happiness/${year}`).then(d=>{
+//     let ts2 = Object.values(d);
+//     let level = parseFloat(ts2) * 20;
 
-    let degrees = 180 - level;
-    let radius = 0.5;
-    let radians = (degrees * Math.PI) / 180;
-    let x = radius * Math.cos(radians);
-    let y = radius * Math.sin(radians);
+//     let degrees = 180 - level;
+//     let radius = 0.5;
+//     let radians = (degrees * Math.PI) / 180;
+//     let x = radius * Math.cos(radians);
+//     let y = radius * Math.sin(radians);
 
-    let mainPath = "M -.0 -0.05 L .0 0.05 L ";
-    let pathX = String(x);
-    let space = " ";
-    let pathY = String(y);
-    let pathEnd = " Z";
-    let path = mainPath.concat(pathX, space, pathY, pathEnd);
+//     let mainPath = "M -.0 -0.05 L .0 0.05 L ";
+//     let pathX = String(x);
+//     let space = " ";
+//     let pathY = String(y);
+//     let pathEnd = " Z";
+//     let path = mainPath.concat(pathX, space, pathY, pathEnd);
 
-    let data = [
-      {
-        type: "scatter",
-        x: [0],
-        y: [0],
-        marker: { size: 12, color: "85000000" },
-        showlegend: false,
-        name: "Happiness",
-        text: Number.parseFloat(ts2).toFixed(2),
-        hoverinfo: "text+name"
-      },
-      {
-        values: [50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50],
-        rotation: 90,
-        text: ["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
-        textinfo: "text",
-        textposition: "inside",
-        marker: {
-          colors: [
-            "rgba(0, 105, 11, .5)",
-            "rgba(10, 120, 22, .5)",
-            "rgba(14, 127, 0, .5)",
-            "rgba(110, 154, 22, .5)",
-            "rgba(170, 202, 42, .5)",
-            "rgba(202, 209, 95, .5)",
-            "rgba(210, 206, 145, .5)",
-            "rgba(232, 226, 202, .5)",
-            "rgba(240, 230, 215, .5)",
-            "rgba(255, 255, 255, 0)"
-          ]
-        },
-        labels: ["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
-        hoverinfo: "label",
-        hole: 0.5,
-        type: "pie",
-        showlegend: false
-      }
-    ];
+//     let data = [
+//       {
+//         type: "scatter",
+//         x: [0],
+//         y: [0],
+//         marker: { size: 12, color: "85000000" },
+//         showlegend: false,
+//         name: "Happiness",
+//         text: Number.parseFloat(ts2).toFixed(2),
+//         hoverinfo: "text+name"
+//       },
+//       {
+//         values: [50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50],
+//         rotation: 90,
+//         text: ["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
+//         textinfo: "text",
+//         textposition: "inside",
+//         marker: {
+//           colors: [
+//             "rgba(0, 105, 11, .5)",
+//             "rgba(10, 120, 22, .5)",
+//             "rgba(14, 127, 0, .5)",
+//             "rgba(110, 154, 22, .5)",
+//             "rgba(170, 202, 42, .5)",
+//             "rgba(202, 209, 95, .5)",
+//             "rgba(210, 206, 145, .5)",
+//             "rgba(232, 226, 202, .5)",
+//             "rgba(240, 230, 215, .5)",
+//             "rgba(255, 255, 255, 0)"
+//           ]
+//         },
+//         labels: ["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
+//         hoverinfo: "label",
+//         hole: 0.5,
+//         type: "pie",
+//         showlegend: false
+//       }
+//     ];
 
-    let layout = {
-      paper_bgcolor:"rgba(0,0,0,0)",
-      plot_bgcolor:"rgba(0,0,0,0)",
-      margin: {
-        l: 1,
-        r: 1,
-        b: 0,
-        t: 75,
-        pad: 1
-      },
-      shapes: [
-        {
-          type: "path",
-          path: path,
-          fillcolor: "850000",
-          line: {
-            color: "850000"
-          }
-        }
-      ],
-      title: "<b>Average Global Happiness</b>",
-      height: 250,
-      width: 350,
-      xaxis: {
-        zeroline: false,
-        showticklabels: false,
-        showgrid: false,
-        range: [-1, 1]
-      },
-      yaxis: {
-        zeroline: false,
-        showticklabels: false,
-        showgrid: false,
-        range: [-1, 1]
-      }
-    };
+//     let layout = {
+//       paper_bgcolor:"rgba(0,0,0,0)",
+//       plot_bgcolor:"rgba(0,0,0,0)",
+//       margin: {
+//         l: 1,
+//         r: 1,
+//         b: 0,
+//         t: 75,
+//         pad: 1
+//       },
+//       shapes: [
+//         {
+//           type: "path",
+//           path: path,
+//           fillcolor: "850000",
+//           line: {
+//             color: "850000"
+//           }
+//         }
+//       ],
+//       title: "<b>Average Global Happiness</b>",
+//       height: 250,
+//       width: 350,
+//       xaxis: {
+//         zeroline: false,
+//         showticklabels: false,
+//         showgrid: false,
+//         range: [-1, 1]
+//       },
+//       yaxis: {
+//         zeroline: false,
+//         showticklabels: false,
+//         showgrid: false,
+//         range: [-1, 1]
+//       }
+//     };
 
-    let GAUGE = document.getElementById("gauge");
-    Plotly.newPlot(GAUGE, data, layout);
-  });
-};
+//     let GAUGE = document.getElementById("gauge");
+//     Plotly.newPlot(GAUGE, data, layout);
+//   });
+// };
 
-const makePie = year => {
-	d3.json(`/api/v1.0/global_terror/metadata/${year}`).then((data)=>{
-		let test = Object.values(data)
-		let values =test[5]
-		console.log(values[1]["weaptype1_txt"])
-		let content = []
-		color = ["#333333",
-				 "#444444",
-				 "#555555",
-				 "#777777",
-				 "#888888",
-				 "#999999",
-				 "#cb2121",
-				 "#830909",
-				 "#111111",
-				 "#646464"
-				]
-		for (i=0;i<test.length;i++){
-			dict = {
-				"label":values[i]["weaptype1_txt"],
-				"value":values[i]["fatalities"]+1,
-				"color":color[i]
-			}
-			content.push(dict)
-		}
-		console.log(Object.entries(content))
+// const makePie = year => {
+// 	d3.json(`/api/v1.0/global_terror/metadata/${year}`).then((data)=>{
+// 		let test = Object.values(data)
+// 		let values =test[5]
+// 		console.log(values[1]["weaptype1_txt"])
+// 		let content = []
+// 		color = ["#333333",
+// 				 "#444444",
+// 				 "#555555",
+// 				 "#777777",
+// 				 "#888888",
+// 				 "#999999",
+// 				 "#cb2121",
+// 				 "#830909",
+// 				 "#111111",
+// 				 "#646464"
+// 				]
+// 		for (i=0;i<test.length;i++){
+// 			dict = {
+// 				"label":values[i]["weaptype1_txt"],
+// 				"value":values[i]["fatalities"]+1,
+// 				"color":color[i]
+// 			}
+// 			content.push(dict)
+// 		}
+// 		console.log(Object.entries(content))
 
-		let pie = new d3pie("pieChart", {
-			"header": {
-				"title": {
-					"text": "Weapons Used",
-					"fontSize": 34,
-					"font": "courier"
-				},
-				"titleSubtitlePadding": 10
-			},
-			"size": {
-				"canvasWidth": 350,
-				"canvasHeight":350,
-				"pieInnerRadius": "90%",
-				"pieOuterRadius": "75%"
-			},
-			"data": {
-				"sortOrder": "random",
-				"content": content
-			},
-			"labels": {
-				"outer": {
-					"format": "label",
-					"pieDistance": 10
-				},
-				"inner": {
-					"format": "none"
-				},
-				"mainLabel": {
-					"color":"#ffffff",
-					"fontSize": 11
-				},
-				"percentage": {
-					"color": "#999999",
-					"fontSize": 11,
-					"decimalPlaces": 0
-				},
-				"value": {
-					"color": "#cccc43",
-					"fontSize": 11
-				},
-				"lines": {
-					"enabled": true,
-					"color": "#777777"
-				},
-				"truncation": {
-					"enabled": true
-				}
-			},
-			"misc": {
-				"colors": {
-					"segmentStroke": "#000000"
-				}
-			}
-		});
-	});
-}
+// 		let pie = new d3pie("pieChart", {
+// 			"header": {
+// 				"title": {
+// 					"text": "Weapons Used",
+// 					"fontSize": 34,
+// 					"font": "courier"
+// 				},
+// 				"titleSubtitlePadding": 10
+// 			},
+// 			"size": {
+// 				"canvasWidth": 350,
+// 				"canvasHeight":350,
+// 				"pieInnerRadius": "90%",
+// 				"pieOuterRadius": "75%"
+// 			},
+// 			"data": {
+// 				"sortOrder": "random",
+// 				"content": content
+// 			},
+// 			"labels": {
+// 				"outer": {
+// 					"format": "label",
+// 					"pieDistance": 10
+// 				},
+// 				"inner": {
+// 					"format": "none"
+// 				},
+// 				"mainLabel": {
+// 					"color":"#ffffff",
+// 					"fontSize": 11
+// 				},
+// 				"percentage": {
+// 					"color": "#999999",
+// 					"fontSize": 11,
+// 					"decimalPlaces": 0
+// 				},
+// 				"value": {
+// 					"color": "#cccc43",
+// 					"fontSize": 11
+// 				},
+// 				"lines": {
+// 					"enabled": true,
+// 					"color": "#777777"
+// 				},
+// 				"truncation": {
+// 					"enabled": true
+// 				}
+// 			},
+// 			"misc": {
+// 				"colors": {
+// 					"segmentStroke": "#000000"
+// 				}
+// 			}
+// 		});
+// 	});
+// }
 
 const makeAssets = year=>{
   d3.json(`/api/v1.0/global_terror/${year}`).then(data =>{
@@ -517,8 +517,8 @@ function init() {
     
     let firstYear = data[0];
     setTimeout(buildMetadata,1000,firstYear);
-    setTimeout(makePie,2000,firstYear);
-    setTimeout(buildGauge, 3000, firstYear);
+    // setTimeout(makePie,2000,firstYear);
+    // setTimeout(buildGauge, 3000, firstYear);
     setTimeout(makeAssets, 4000,firstYear);
   });
 }
@@ -528,14 +528,14 @@ function optionChanged(newYear) {
     map.remove()
     d3.select("#mapbox").append("div")
       .attr("id", "map");
-    d3.select("#pieChart").html("");
-    if(newYear>2004){
-      d3.select("#gauge").html("");
-      setTimeout(buildGauge, 1, newYear);
-    }
+    // d3.select("#pieChart").html("");
+    // if(newYear>2004){
+    //   d3.select("#gauge").html("");
+    //   setTimeout(buildGauge, 1, newYear);
+    // }
     setTimeout(makeAssets, 3000, newYear)
     setTimeout(buildMetadata,1000,newYear);
-    setTimeout(makePie,2000,newYear);
+    // setTimeout(makePie,2000,newYear);
     
 };
 
