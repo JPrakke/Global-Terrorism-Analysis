3efa14140882b825faa579c096555840dc9a2dc2
jprakke <JoshPrakke@gmail.com>
2019-03-08 20:11:44 -0500

    retrofit queries and updated dash.js

--- GTA/back_end/urls.py
+++ GTA/back_end/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.api, name="API"),
+    path('years/', views.years, name="year list"),
+    path('metadata/<year_input>/', views.metadata, name ="metadata list" ),
+    path('coord/<year_input>/', views.location, name="coords for map"),
 ]
