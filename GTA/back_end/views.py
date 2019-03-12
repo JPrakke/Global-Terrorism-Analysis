40965a7779687bde96a554e88475511c4d82f536
jprakke <JoshPrakke@gmail.com>
2019-03-12 01:20:32 -0400

    added api write up

--- GTA/back_end/views.py
+++ GTA/back_end/views.py
@@ -6,7 +6,14 @@ from django.db.models import Sum ,Count
 
 
 def api(request):
-    return HttpResponse("Hello, API!")
+    availible_routes = [
+        'api/populate/',
+        'api/metadata/2000/All',
+        'api/coord/2000/All',
+        'api/attack-info/2000/All'
+    ]
+    context = {"availible_routes":availible_routes}
+    return render(request, 'back_end/api.html',  context)
 
 def populate(request):
     """ returns list of availible years and top 10 terrorist groups"""
@@ -27,8 +34,6 @@ def populate(request):
             }
     for value in GlobalTerrorism.objects.values('iyear').distinct():
         list["year"].append(value["iyear"])
-    # for value in GlobalTerrorism.objects.values('gname').annotate(tcount=Count('index1')).order_by('-tcount')[1:11]:
-    #     list["group"].append(value['gname'])
     return JsonResponse(list)
 
 def metadata(request, year_input, group_input):
@@ -64,5 +69,18 @@ def location(request, year_input, group_input):
             list.append(value)
     else:
         for value in GlobalTerrorism.objects.values('latitude', 'longitude').filter(iyear=year_input,gname=group_input):
+            list.append(value)
+    return JsonResponse(list, safe=False)
+
+def attack_info(request, year_input, group_input):
+    """ returns all information on attacks by year """
+    list = []
+    if group_input == "All":
+        response = GlobalTerrorism.objects.filter(iyear=year_input).values()
+        for value in response:
+            list.append(value)
+    else:
+        response = GlobalTerrorism.objects.filter(iyear=year_input,gname=group_input).values()
+        for value in response:
             list.append(value)
     return JsonResponse(list, safe=False)
